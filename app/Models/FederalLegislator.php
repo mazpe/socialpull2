2c064a1c6ef04f22a8a6821f51814d190ce89c2e
Lester Ariel Mesa <lesterm@lestermesa.me>
2017-09-28 12:06:29 -0400

    created relationship between social post and federal legislators

--- app/Models/FederalLegislator.php
+++ app/Models/FederalLegislator.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FederalLegislator extends Model
 {
-    //
+    /**
+     * Federal Legislator model has many Social Posts
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function social_posts()
+    {
+        return $this->hasMany('App\Models\SocialPost');
+    }
 }
--- app/Models/SocialPost.php
+++ app/Models/SocialPost.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialPost extends Model
 {
-    //
+    /**
+     * Social Post model belongs to a Federal Legislator.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function federalLegislator()
+    {
+        return $this->belongsTo('App\Models\FederalLegislator');
+    }
 }
